a6bf9c1143858c222597f26ce84b99f60ae89e30
Andreas Hennings <andreas@dqxtech.net>
2013-07-09 23:39:46 +0200

    Some reusable Util stuff.

--- src/Krautoload/Util.php
+++ src/Krautoload/Util.php
@@ -2,6 +2,9 @@
 
 namespace Krautoload;
 
+/**
+ * Collection of standalone static methods with no non-obvious side effects.
+ */
 class Util {
 
   /**
@@ -27,5 +30,112 @@ class Util {
 
   static function classIsDefined($class) {
     return class_exists($class, FALSE) || interface_exists($class, FALSE) || trait_exists($class, FALSE);
+  }
+
+  /**
+   * Check if a file exists, considering the full include path.
+   *
+   * @param string $file
+   *   The filepath
+   * @return boolean
+   *   TRUE, if the file exists somewhere in include path.
+   *   FALSE, otherwise.
+   */
+  static function fileExistsInIncludePath($file) {
+    if (function_exists('stream_resolve_include_path')) {
+      // Use the PHP 5.3.1+ way of doing this.
+      return (FALSE !== stream_resolve_include_path($file));
+    }
+    elseif ($file{0} === DIRECTORY_SEPARATOR) {
+      // That's an absolute path already.
+      return file_exists($file);
+    }
+    else {
+      // Manually loop all candidate paths.
+      foreach (explode(PATH_SEPARATOR, get_include_path()) as $base_dir) {
+        if (file_exists($base_dir . DIRECTORY_SEPARATOR . $file)) {
+          return TRUE;
+        }
+      }
+      return FALSE;
+    }
+  }
+
+  /**
+   * Check if a file exists, considering the full include path.
+   *
+   * @param string $file
+   *   The filepath
+   * @return boolean|string
+   *   The resolved file path, if the file exists in the include path.
+   *   FALSE, otherwise.
+   */
+  static function findFileInIncludePath($file) {
+    if (function_exists('stream_resolve_include_path')) {
+      // Use the PHP 5.3.1+ way of doing this.
+      return stream_resolve_include_path($file);
+    }
+    elseif ($file{0} === DIRECTORY_SEPARATOR) {
+      // That's an absolute path already.
+      return file_exists($file) ? $file : FALSE;
+    }
+    else {
+      // Manually loop all candidate paths.
+      foreach (explode(PATH_SEPARATOR, get_include_path()) as $base_dir) {
+        if (file_exists($base_dir . DIRECTORY_SEPARATOR . $file)) {
+          return $base_dir . DIRECTORY_SEPARATOR . $file;
+        }
+      }
+      return FALSE;
+    }
+  }
+
+  /**
+   * @param string $string
+   *   The original string, that we want to explode.
+   * @param boolean $lowercase
+   *   should the result be lowercased?
+   * @param string $example_string
+   *   Example to specify how to deal with multiple uppercase characters.
+   *   Can be something like "AA Bc" or "A A Bc" or "AABc".
+   * @param boolean $glue
+   *   Allows to implode the fragments with sth like "_" or "." or " ".
+   *   If $glue is FALSE, it will just return an array.
+   *
+   * @return array|string
+   *   An indexed array of pieces, if $glue is FALSE.
+   *   A glued string, if $glue is a string.
+   */
+  static function camelCaseExplode($string, $lowercase = TRUE, $example_string = 'AA Bc', $glue = FALSE) {
+    static $regexp_available = array(
+      '/([A-Z][^A-Z]*)/',
+      '/([A-Z][^A-Z]+)/',
+      '/([A-Z]+[^A-Z]*)/',
+    );
+    static $regexp_by_example = array();
+    if (!isset($regexp_by_example[$example_string])) {
+      $example_array = explode(' ', $example_string);
+      foreach ($regexp_available as $regexp) {
+        if (implode(' ', preg_split(
+            $regexp,
+            str_replace(' ', '', $example_string),
+            -1,
+            PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE
+          )) == $example_string) {
+          break;
+        }
+      }
+      $regexp_by_example[$example_string] = $regexp;
+    }
+    $array = preg_split(
+      $regexp_by_example[$example_string],
+      $string,
+      -1,
+      PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE
+    );
+    if ($lowercase) {
+      $array = array_map('strtolower', $array);
+    }
+    return (FALSE !== $glue) ? implode($glue, $array) : $array;
   }
 }
