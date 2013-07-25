a0902d6fadab3ffc19aec1531dedab142a9a4088
Andreas Hennings <andreas@dqxtech.net>
2013-07-25 05:22:13 +0200

    Docblock comment.

--- src/Krautoload/NamespaceInspector/Interface.php
+++ src/Krautoload/NamespaceInspector/Interface.php
@@ -7,7 +7,7 @@ interface NamespaceInspector_Interface extends ClassLoader_Interface {
   /**
    * @param InjectedAPI_ClassFileVisitor_Interface $api
    * @param array $namespaces
-   * @param $recursive
+   * @param bool $recursive
    */
   public function apiVisitClassFiles(InjectedAPI_ClassFileVisitor_Interface $api, array $namespaces, $recursive);
 }
