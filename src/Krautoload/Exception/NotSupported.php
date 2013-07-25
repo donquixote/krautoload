5ba54a50c15c5e899c4c34ac94b1879ac77a9fe7
Andreas Hennings <andreas@dqxtech.net>
2013-07-25 04:25:55 +0200

    Add Exception classes.

---
+++ src/Krautoload/Exception/NotSupported.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: lemonhead
+ * Date: 7/24/13
+ * Time: 11:55 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace Krautoload;
+
+
+class Exception_NotSupported extends \Exception {
+
+}
